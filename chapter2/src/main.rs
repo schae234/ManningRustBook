2b2ff8566e6df6f752d2eb66b12471f6713c95db
Rob <rob@linkage.io>
2020-03-30 08:00:06 -0600

    checkpoint

--- chapter2/src/main.rs
+++ chapter2/src/main.rs
@@ -51,47 +51,49 @@ fn listing_3_3(){
 }
 
 
-#[derive(Debug)]
-struct File {
-  name: String,
-  data: Vec<u8>,
-}
+fn listing_3_6(){
+    #[derive(Debug)]
+    struct File {
+    name: String,
+    data: Vec<u8>,
+    }
+    
+    fn open(f: &mut File) -> bool {
+        true
+    }
+    
+    fn close(f: &mut File) -> bool {
+        true
+    }
+    
+    fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
+        let mut tmp = f.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        read_length
+    }
+    
+    fn main() {
+        // Create the file struct
+        let mut f2 = File {
+            name: String::from("2.txt"),
+            data: vec![114, 117, 115, 116, 33],
+        };
+    
+        // allocate a buffer
+        let mut buffer: Vec<u8> = vec![];
+    
+        // the open and close functions are just place
+        // holders right now
+        open(&mut f2);
+        let f2_length = read(&f2, &mut buffer);
+        close(&mut f2);
+    
+        let text = String::from_utf8_lossy(&buffer);
 
-fn open(f: &mut File) -> bool {
-  true
-}
-
-fn close(f: &mut File) -> bool {
-  true
-}
-
-fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
-  let mut tmp = f.data.clone();
-  let read_length = tmp.len();
-  save_to.reserve(read_length);
-  save_to.append(&mut tmp);
-  read_length
-}
-
-fn main() {
-  // Create the file struct
-  let mut f2 = File {
-    name: String::from("2.txt"),
-    data: vec![114, 117, 115, 116, 33],
-  };
-
-  // allocate a buffer
-  let mut buffer: Vec<u8> = vec![];
-
-  // the open and close functions are just place
-  // holders right now
-  open(&mut f2);
-  let f2_length = read(&f2, &mut buffer);
-  close(&mut f2);
-
-  let text = String::from_utf8_lossy(&buffer);
-
-  println!("{:?}", f2);
-  println!("{} is {} bytes long", &f2.name, f2_length);
-  println!("{}", text)
+        println!("{:?}", f2);
+        println!("{} is {} bytes long", &f2.name, f2_length);
+        println!("{}", text)
+    }
 }
